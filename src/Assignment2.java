2a71066619d80c8e9b6a83d058c3b73ef04a2f9f
mokong03 <mark.limon03@gmail.com>
2022-10-02 09:48:36 -0700

    3rd Commit and Final.

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -22,11 +22,11 @@ public class Assignment2 {
 		String regex1 = "[Pp]ANID.+";///PANID
 		
 		
-		String regex2 = "[0-9]{6}[a-z]{2}[a-z0-9]{7}[a-z0-9]{1}";
+		String regex2 = "[0-9]{6}[a-z]{2}[a-z0-9]{7}[a-z0-9]{1}";//MACID
 		
-		String regex3 = "[0-9]{6}[a-z]{2}[a-z0-9]{7}[a-z0-9]{1}";
+		String regex3 = "[0-9]{6}[a-z]{2}[a-z0-9]{7}[a-z0-9]{1}";//MACID
 		
-		String regex4 = "[-0-9.] {1,7}";
+		String regex4 = "-[0-9]*\\.[0-9]*";// RF RSSI M
 		
 		Pattern pattern1 = Pattern.compile(regex1);//PANID
 		Pattern pattern2 = Pattern.compile(regex2);//MACID
@@ -42,19 +42,23 @@ public class Assignment2 {
 		Matcher matcher4 = pattern3.matcher(input);
 		
 		Matcher matcher5 = pattern3.matcher(input);//displaying the MACID with RF RSSI M
-		Matcher matcher6 = pattern4.matcher(input);
+		Matcher matcher6 = pattern4.matcher(input);//displaying the RF RSSI M
 		
-		System.out.println(Pattern.matches("[0-9]{6}[a-z]{2}[a-z0-9]{7}[a-z0-9]{1}","000781fe00002f76"));
+		//This is for troubleshooting purposes only
+		//System.out.println(Pattern.matches("[0-9]{6}[a-z]{2}[a-z0-9]{7}[a-z0-9]{1}","000781fe00002f76"));
 		//000781fe0000614e , 000781fe0000326f , 000781fe00002f76 , 000781fe0000313e
-		System.out.println(Pattern.matches("^-?[0-9]", "-54"));///("^-[0-9]{2,6}", "-45"));
+		//System.out.println(Pattern.matches("-[0-9]*\\.[0-9]*", "-25.5"));///("^-[0-9]{2,6}", "-45"));
+		
 				
+		
+		//PANID
 		while (matcher2.find()) {
 			String match2 = matcher2.group();
 			countPANids ++;
 		}
 		
 		System.out.println("- List of PANIDs (Total of "+countPANids + (")"));
-		
+		//List of PANID
 		while(matcher1.find()) {
 			
 			String match = matcher1.group();
@@ -62,29 +66,28 @@ public class Assignment2 {
 			System.out.println(match);
 			
 		}
-		
+		//MACID
 		while (matcher3.find()) {
 			String match3 = matcher3.group();
 			countMAC++;
 		}
 		
 		System.out.println("- List of MAC Addresses (Total of "+countMAC + (")"));
-		
+		// List of MACID
 		while(matcher4.find()) {
 			String match = matcher4.group();
 			System.out.println(match);
 		}
 								
 		System.out.println("- List of RF_RSSI_M values for each MAC address  ");
-
-		while(matcher5.find()) {
+		
+		//List of MACID plus RF RSSI M
+		while(matcher5.find() && matcher6.find()) {
 			String match = matcher5.group();
-			System.out.println(match);
+			String match2 = matcher6.group();
+			System.out.println(match + " " + match2);
 		}
-		/*while(matcher6.find()) {
-			String match = matcher6.group();
-			System.out.println(match);
-		}*/
+		
 
 
 	}
